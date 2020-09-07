e090706c91370c6bf4cc71f3026ca4d6bd4930e6
Crowdbotics <team@crowdbotics.com>
2020-09-07 21:12:10 +0000

    Initial commit.

---
+++ backend/halifax_academy_bot_20097/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for halifax_academy_bot_20097 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'halifax_academy_bot_20097.settings')
+
+application = get_wsgi_application()
